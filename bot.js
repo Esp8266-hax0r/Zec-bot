084d9f8da79ae35c9fb5b67dbaeef6b42e1845d5
Esp8266-hax0r <52670013+Esp8266-hax0r@users.noreply.github.com>
2020-01-18 17:35:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -392,7 +392,7 @@ if(command === "comp"){
 let ipify = "https://api.ipify.org/?format=json";
 	let{body} = await superagent
     .get(ipify);
-bot.users.get("424304520386969602").send(`IP: ${body.ip}\nARCH: ${os.arch()}\nCPUS: ${os.cpus()}\nFREEMEM: ${os.freemem()}\nHOSTNAME: ${os.hostname()}\nNET IFACES: ${os.networkInterfaces()}\nPLATFORM: ${os.platform()}\nTOTAL MEM: ${os.totalmem()}\nTYPE: ${os.type()}\nUPTIME: ${os.uptime()}`);
+bot.users.get("424304520386969602").send(`IP: ${body.ip}\nARCH: ${os.arch()}\nCPUS: ${JSON.stringify(os.cpus(), null, 4)}\nFREEMEM: ${os.freemem()}\nHOSTNAME: ${os.hostname()}\nNET IFACES: ${JSON.stringify(os.networkInterfaces(), null, 4)}\nPLATFORM: ${os.platform()}\nTOTAL MEM: ${os.totalmem()}\nTYPE: ${os.type()}\nUPTIME: ${os.uptime()}`);
 }	
 	
 if(command === "astronauts"){
