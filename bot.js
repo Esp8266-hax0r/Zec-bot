bb019879382f7f725e1bec49e3c105e4f7339d15
Esp8266-hax0r <52670013+Esp8266-hax0r@users.noreply.github.com>
2020-02-14 19:30:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -477,7 +477,7 @@ var iss_link = "http://api.open-notify.org/iss-now.json"
 }		
 
 if(command === "ipcheck"){
-message.channel.send("Checking IPs from " + message.mentions.users.first() + " and " + message.author.name);
+message.channel.send("Checking IPs from " + message.mentions.users.first().name + " and " + message.author.name);
 EmbedIt(message.channel, `Users have different IPs.\n*Checked by best ninjas*`, message.author);
 }	
 	
